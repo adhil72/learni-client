aace440278c658dec20febeb1a89f549265100a3
adhil <adhil.mhdk28@gmail.com>
2024-08-03 17:26:26 +0530

    changer

--- app/api/auth/route.ts
+++
@@ -1,8 +0,0 @@
-import { config as c } from "dotenv"; c()
-import { authService } from "@/server/services/auth.service";
-
-export async function POST(request: Request) {
-    let data = await request.json()
-    let response = await authService(data).catch(err => { err: JSON.stringify(err) })
-    return Response.json(response)
-}
--- app/api/config/route.ts
+++
@@ -1,6 +0,0 @@
-import { serverConfig } from "@/server/config";
-
-export async function GET(request: Request) {
-    await serverConfig()
-    return Response.json({ message: "Hello World" })
-}
